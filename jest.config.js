4c35686f300bb21b89088ef4d584529c1ca10c29
Taliesin67 <Taliesin67@users.noreply.github.com>
2025-05-13 19:06:49 +0000

    Update Jest configuration for comprehensive testing

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,14 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   verbose: true,
   collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
-  testEnvironment: 'node',
+  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: 'coverage',
   roots: ['<rootDir>/tests'],
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  }
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
