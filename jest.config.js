0c56ee9ad2900c7a1c87931d1c8ff16733a2321a
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-05-13 23:35:32 +0000

    Enhance Jest configuration with comprehensive settings

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 /** @type {import('jest').Config} */
 module.exports = {
+  // Root directory for tests
+  rootDir: '.',
+
   // Test environment
   testEnvironment: 'node',
   
   // File extensions to test
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   
   // Test match patterns
   testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[tj]s?(x)'
+    '**/tests/**/*.test.js',
+    '**/tests/**/*.spec.js'
   ],
   
   // Coverage configuration
@@ -18,10 +21,10 @@ module.exports = {
   coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
     }
   },
   
@@ -30,6 +33,9 @@ module.exports = {
   
   // Transform configuration
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest'
-  }
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+
+  // Verbose output for better debugging
+  verbose: true
 };
