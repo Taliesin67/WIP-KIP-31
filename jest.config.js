0de5c4c023caf9abc15cc7d3c872f3ccebf0d86b
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-05-14 07:27:00 +0000

    Create Jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
